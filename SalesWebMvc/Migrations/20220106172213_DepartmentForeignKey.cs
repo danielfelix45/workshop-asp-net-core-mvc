336beabf887ccec241a5af4d3009638f3c7321af
Daniel Felix <felixdaniel9@gmail.com>
2022-01-06 14:29:08 -0300

    Foreign key not null (referential integrity)

---
+++ SalesWebMvc/Migrations/20220106172213_DepartmentForeignKey.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Sellers",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Sellers",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMvc.Models
 
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() {}
